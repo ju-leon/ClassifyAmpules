c6b3cc655bc31ec6f2093eaeeeae0309c42e9655
Yannick Funk <yannickfunk@yahoo.de>
2020-07-03 14:25:57 +0200

    remove conv layer

--- train.py
+++ train.py
@@ -35,10 +35,6 @@ def get_classifier(input_shape):
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(256, (4, 4)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
     model.add(Conv2D(256, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
