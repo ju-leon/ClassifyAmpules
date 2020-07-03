8960fa854897f950cc28b3908fa620c40e303379
Leon Jungemeyer <leon.jungemeyer@me.com>
2020-07-03 16:30:28 +0200

    Make confusion matrix pretty

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 import argparse
 
 # plots
+import itertools
 import matplotlib.pyplot as plt
 
 
@@ -81,8 +82,41 @@ def plot_confusion_matrix(validation_generator):
     cm = confusion_matrix(classes, y_pred)
     cr = classification_report(classes, y_pred)
     print(cr)
-    print(cm)
+
+    plot_confusion_matrix_pretty(cm, classes=["bad_piece", "good_piece"])
+
     print(validation_generator.class_indices)
+
+def plot_confusion_matrix_pretty(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+
+    #This function prints and plots the confusion matrix.
+    #Normalization can be applied by setting `normalize=True`.
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
 def main():
